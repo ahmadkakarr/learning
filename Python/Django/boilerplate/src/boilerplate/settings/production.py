2a5536c3420f23c7def6769b65bbae89b9683653
Ahmed Hassan Khan <mc180200919@vu.edu.pk>
2020-05-04 14:10:21 +0500

    removed comments and spaces

--- Python/Django/boilerplate/src/boilerplate/settings/production.py
+++ Python/Django/boilerplate/src/boilerplate/settings/production.py
@@ -1,11 +1,9 @@
 from .base import *
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 ALLOWED_HOSTS = ['ip-address', 'www.your-website.com']
 
 
-# Application definition
 
 INSTALLED_APPS += [
     
