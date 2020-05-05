3464d1c54f36f495a5273d199421391263c90343
Ahmed Hassan Khan <mc180200919@vu.edu.pk>
2020-05-06 04:29:48 +0500

    i don't know what i am tracking

---
+++ Javascript/Udemy - Javascript Tutorial and Projects CourseUdemy - Javascript Tutorial and Projects Course/DOM/app.js
@@ -0,0 +1,74 @@
+// Select the element or group of elements the we want
+// Decide the effect we want to apply to the selection
+
+/*
+document.body.style.color = 'green';
+
+document.querySelector('h2').style.color = 'red';
+
+
+
+//window object: use chrome browser
+console.log(window)
+//to view the document
+console.log(document)
+// to find the object properties and methods
+console.dir(document)
+
+const btns = document.getElementById('btn')
+btns.style.color = 'red';
+btns.style.border = 'solid';
+
+const name = btns.nodeName;
+const css  = btns.style;
+
+console.log(btns);
+console.log(name);
+console.log(css);
+
+*/
+
+/*
+
+// getElementsByTagName('tagname');
+// node-list =  array-like object
+// index, length property but not array methods
+
+const headings = document.getElementsByTagName('h2');
+headings[0].style.color = 'red';
+// console.log(headings.length);
+
+const items = document.getElementsByTagName('li');
+
+//for each dose not work on node-list
+// items.forEach(function(item) {
+//   console.log(item);
+// });
+items[2].style.color = 'orange';
+
+// converting note-list into an array
+const betterItems = [...items];
+
+// =============================================
+// for selecting each item in array with forEach
+// =============================================
+betterItems.forEach(function(item) {
+  // console.log(item);
+});
+console.log(items);
+console.log(betterItems);
+
+const headings2 = document.getElementsByTagName('h2');
+console.log(headings2);
+console.log(headings2.length);
+
+headings2[1].style.color = 'blue';
+headings2[2].style.color = 'green';
+
+*/
+
+
+
+
+
+
---
+++ Javascript/Udemy - Javascript Tutorial and Projects CourseUdemy - Javascript Tutorial and Projects Course/javascript basics/app.js
@@ -0,0 +1,30 @@
+
+/*
+===========================================================================
+//functions without name are call anonymous functions, as in example below.
+===========================================================================
+document.querySelectorAll(".btn").forEach(function (item){
+    item.addEventListener('click', function () {
+        alert('this is from external javascript')
+      })
+})
+=============================
+//ES6 syntax: arrow functions
+=============================
+document.querySelectorAll('.btn').forEach((item)=>{
+    item.addEventListener('click', () => {
+        alert('this alert is form arrow function. External Javascript')
+    })
+})
+*/
+
+/*
+============================================================
+//Testing Methods: these functions are used to test our code
+============================================================
+document.write('Hello World');
+alert('Hello World');
+console.log('Hello World');
+*/
+
+//ctrl+shift+j to open console window in browsers
